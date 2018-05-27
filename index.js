13ff090aea3bd1efe9b7e0fee0b9f16093783080
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 /* eslint-disable no-undefined */
-import type from "@unction/type"
-import fromIteratorToArray from "@unction/fromiteratortoarray"
-
+import type from "@unction/type";
+import fromIteratorToArray from "@unction/fromiteratortoarray";
 const fromMapping = {
-  Array: (array: ArrayType): Array<[number, ValueType]> => array.reduce((pairs: Array<[number, ValueType]>, value: ValueType, index: number): Array<[number, ValueType]> => [...pairs, [index, value]], []),
-  Object: (object: ObjectType): Array<[string, ValueType]> => Object.entries(object),
-  Set: (set: SetType): Array<[void, ValueType]> => fromIteratorToArray(set.values()).map((value: mixed): [void, ValueType] => [undefined, value]),
-  Map: (map: MapType): Array<[KeyType, ValueType]> => fromIteratorToArray(map.entries()),
-}
-
-export default function fromFunctorToPairs (pairableValue: ArrayType | ObjectType | SetType | MapType): Array<[?KeyType, ?ValueType]> {
+  Array: array => array.reduce((pairs, value, index) => [...pairs, [index, value]], []),
+  Object: object => Object.entries(object),
+  Set: set => fromIteratorToArray(set.values()).map(value => [undefined, value]),
+  Map: map => fromIteratorToArray(map.entries())
+};
+export default function fromFunctorToPairs(pairableValue) {
   if (fromMapping[type(pairableValue)]) {
-    return fromMapping[type(pairableValue)](pairableValue)
+    return fromMapping[type(pairableValue)](pairableValue);
   }
 
-  throw new Error(`fromFunctorToPairs doesn't know how to handle ${type(pairableValue)}`)
+  throw new Error(`fromFunctorToPairs doesn't know how to handle ${type(pairableValue)}`);
 }
