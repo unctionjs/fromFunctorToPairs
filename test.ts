46c3655950f5b8647d6d403d2cdaa54e5cc32d70
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:46 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-undefined */
 import fromFunctorToPairs from "./index";
 
-test(() => {
+test("works", () => {
   expect(fromFunctorToPairs({
     aaa: "a",
     bbb: "b",
@@ -9,18 +9,18 @@ test(() => {
   })).toEqual([["aaa", "a"], ["bbb", "b"], ["ccc", "c"]]);
 });
 
-test(() => {
+test("works", () => {
   expect(fromFunctorToPairs(["a", "b", "c"])).toEqual([[0, "a"], [1, "b"], [2, "c"]]);
 });
 
-test(() => {
+test("works", () => {
   expect(fromFunctorToPairs(new Map([["aaa", "a"], ["bbb", "b"], ["ccc", "c"]]))).toEqual([["aaa", "a"], ["bbb", "b"], ["ccc", "c"]]);
 });
 
-test(() => {
+test("works", () => {
   expect(fromFunctorToPairs(new Set(["a", "b", "c"]))).toEqual([[undefined, "a"], [undefined, "b"], [undefined, "c"]]);
 });
 
-test(() => {
+test("works", () => {
   expect(() => fromFunctorToPairs(true)).toThrow();
 });
